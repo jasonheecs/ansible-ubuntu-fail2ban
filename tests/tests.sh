10b6c4467cd3a412e9d4290c4c4c0f7f07361c83
Jason Hee <jasonheecs@gmail.com>
2018-03-16 19:58:24 +0800

    Updated travis build

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,6 +7,6 @@ idempotence=$(mktemp)
 ansible-container run 2>&1 | tee -a ${idempotence}
 tail ${idempotence} | grep 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 
-docker exec -it fail2bantests_trusty_1 service fail2ban start | grep '\[ OK \]' && (echo 'Fail2Ban started OK!' && exit 0) || (echo 'Fail2Ban not started~' && exit 1)
+docker exec -it fail2bantests_trusty_1 service fail2ban start | grep '\[ OK \]' && (echo 'Fail2Ban started OK!' && exit 0) || (echo 'Fail2Ban not started!' && exit 1)
 
 ansible-container destroy
