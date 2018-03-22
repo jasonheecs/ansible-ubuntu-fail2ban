5afbf15dd072584809f76e82ba9030565349fc99
Jason Hee <jasonheecs@gmail.com>
2018-03-22 18:24:09 +0800

    Added testing for xenial

--- Makefile
+++ Makefile
@@ -1,9 +1,25 @@
+.PHONY: run-tests
+run-tests: test-trusty test-xenial
+
+.PHONY: test-trusty
+test-trusty:
+	mv tests/container-trusty.yml tests/container.yml
+	make test
+	cd tests && ./tests.sh trusty
+	mv tests/container.yml tests/container-trusty.yml
+
+.PHONY: test-xenial
+test-xenial:
+	mv tests/container-xenial.yml tests/container.yml
+	make test
+	cd tests && ./tests.sh xenial
+	mv tests/container.yml tests/container-xenial.yml
+
 .PHONY: test
 test:
 	mkdir -p tests/roles/ansible-ubuntu-fail2ban/defaults/
 	rsync -avzh --relative ./defaults ./handlers ./tasks ./templates tests/roles/ansible-ubuntu-fail2ban/
 	ansible-playbook tests/test.yml --syntax-check
-	cd tests && ./tests.sh
 
 .PHONY: clean
 clean:
--- tests/tests.sh
+++ tests/tests.sh
@@ -9,8 +9,8 @@ idempotence=$(mktemp)
 ansible-container run 2>&1 | tee -a ${idempotence}
 tail ${idempotence} | grep 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 
-docker exec -it fail2bantests_trusty_1 service fail2ban stop
-docker exec -it fail2bantests_trusty_1 rm -rf /var/run/fail2ban/fail2ban.sock
-docker exec -it fail2bantests_trusty_1 service fail2ban start | grep '\[ OK \]' && (echo 'Fail2Ban started OK!' && exit 0) || (echo 'Fail2Ban not started!' && exit 1)
+docker exec -it fail2bantests_"${1}"_1 service fail2ban stop
+docker exec -it fail2bantests_"${1}"_1 rm -rf /var/run/fail2ban/fail2ban.sock
+docker exec -it fail2bantests_"${1}"_1 service fail2ban start | grep '\[ OK \]' && (echo 'Fail2Ban started OK!' && exit 0) || (echo 'Fail2Ban not started!' && exit 1)
 
 ansible-container destroy
